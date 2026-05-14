7952e08600cc5f47757e9c719d8a3e395cef99fe
krish1124007 <parmarkrish005@gmail.com>
2026-05-15 00:20:14 +0530

    update the live url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000'; // Adjust for your backend URL
+const API_BASE_URL = 'https://tankarsolutions.tech'; // Adjust for your backend URL
 const API2 = "api"
 
 const api = axios.create({
